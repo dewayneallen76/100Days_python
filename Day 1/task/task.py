cf2408f84189ff976b9a1836978a712af026dc8b
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-05 16:27:49 -0400

    using the input function

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,13 +1,4 @@
-# Write your code below this line 👇
+print("My name is" + " " + "Angela")
 
-# String Manipulation
-# \n - new line
-# use \n to add another line of "Hello World"
-
-print("Hello World!\nHello World!\nHello World!")
-
-# use + to concatenate strings or combine
-print("Hello" + "Angela")
-
-# remember to use a space at the end or beginning of a string to put a space when concatenating
-print("Hello" + " Angela")
+#The input() function is a prompt for user input.
+print("Hello " + input("What is your name?") + "!")
