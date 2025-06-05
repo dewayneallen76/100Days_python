48c87ae02d45e1800062dcb3449d56559ece962b
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-05 16:09:30 -0400

    string manipulation lesson

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,13 @@
 # Write your code below this line 👇
-print("Hello world!")
+
+# String Manipulation
+# \n - new line
+# use \n to add another line of "Hello World"
+
+print("Hello World!\nHello World!\nHello World!")
+
+# use + to concatenate strings or combine
+print("Hello" + "Angela")
+
+# remember to use a space at the end or beginning of a string to put a space when concatenating
+print("Hello" + " Angela")
