3fbbb57e7ee225aa95513fb345f6661b65862784
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-05 17:21:48 -0400

    day 1: inputs, variables, variable naming, band name generator project

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,4 @@
-print("My name is" + " " + "Angela")
-
-#The input() function is a prompt for user input.
-print("Hello " + input("What is your name?") + "!")
+print("Create a name for your band.")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your first pet?\n")
+print("You could name your band " + city + " " + pet + "!")
