1a46e54e193f45364dde588700215658b7d3e89c
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-06 17:34:50 -0400

    mathematical operations

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,19 @@
 print("My age: " + str(12))
+
+print(3 + 5) # + add
+print(6 - 3) # - subtract
+print(3 * 5) # * multiply
+print(13 / 5) # / divide may have float as output
+print(13 // 5) # // divide with int output
+print(2 ** 2) # exponent
+
+# PEMDAS mathematical operations are done in this order and LEFT TO RIGHT
+# Parentheses ()
+# Exponents   **
+# Multiplication * OR /
+# Division / OR *
+# Addition + OR -
+# Subtraction - OR +
+
+print(3 * 3 + 3 / 3 - 3)
+
