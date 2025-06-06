d1cfcb2156aa9779222586df074e2642c7c58ff7
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-06 15:25:31 -0400

    primitive data types

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,23 @@
 len("Hello")
+
+# Subscripting - pulling a particular element out of a string
+# uses []
+# Ex: To get the first letter of the string "Hello":
+print("Hello"[0])
+
+# String
+print("123" + "345") #Concatenation
+
+# Integer = whole number
+print(123 + 345)
+
+# Large integers
+# Underscore can be used to make large numbers more readable
+print(123_456_789)
+
+# Float = floating point number or number with a decimal
+print(3.1459)
+
+# Boolean = True or False
+print(True)
+print(False)
