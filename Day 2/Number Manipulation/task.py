e7f17c597383b6004838059b505b91ace985b0c9
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-07 09:45:55 -0400

    Day 2 project tip calculator

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,20 @@
 bmi = 84 / 1.65 ** 2
+
+print(bmi)
+
+# Flooring a number: removing the digits after the decimal
+print(int(bmi))
+
+# Rounding a number: rounding up or down based on the first digit after the decimal
+print(round(bmi))
+
+# Rounding a number and specifying the number of digits after the decimal
+print(round(bmi, 2))
+
+score = 1
+score += 2
+print(score)
+
+#In Python, we can use f-strings to insert a variable or an expression into a string.
+age = 12
+print(f"I am {age} years old")
