9766a196daf4f11f7182210a02d82ee320301bfa
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-09 17:59:58 -0400

    Day 3: update rollercoaster to use a logical operator for ages 45 to 55

--- Day 3/Logical Operators/task.py
+++ Day 3/Logical Operators/task.py
@@ -11,6 +11,9 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+# Use a logical operator to check age 45-55 and they ride free.
+    elif age >= 45 and age <= 55:
+        print("Everything will be OK. You can ride for free!")
     else:
         bill = 12
         print("Adult tickets are $12.")
