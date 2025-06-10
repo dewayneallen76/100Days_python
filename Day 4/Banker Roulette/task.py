300e22dac35a0678f5671cd64233f13b5a6d41d4
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-10 15:30:12 -0400

    Day 4: bank roulette challenge

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,9 @@
+import random
+# My solution
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+print(friends)
+print(random.choice(friends))
+
+# Another solution
+random_index = random.randint(0, 4)
+print(friends[random_index])
