d6154e8709bf0945aa0699e3c3bd7556ae64ea20
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-10 16:19:12 -0400

    Day 4: rock, paper, scissors game challenge. got the concept first, but needed to refactor using lists, etc. did have to look at solution for hints.

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,7 @@
+import random
+# rock wins against scissors
+# scissors wins against paper
+# paper wins against rock
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +28,26 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_images = [rock, paper, scissors]
+
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+
+if player_choice >= 0 and player_choice <=2:
+    print(game_images[player_choice])
+
+print("Computer chose:")
+computer_choice = random.randint(0,2)
+print(game_images[computer_choice])
+
+if player_choice >= 3 or player_choice < 0:
+    print("You typed an invalid number. You loser!")
+elif player_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and player_choice == 2:
+    print("You lose!")
+elif computer_choice > player_choice:
+    print("You lose!")
+elif player_choice > computer_choice:
+    print("You win!")
+elif player_choice == computer_choice:
+    print("It's a draw!")
