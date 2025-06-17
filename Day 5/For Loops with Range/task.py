456c9ad482a2d56261a10893bdeaabc76aa30174
Dewayne Allen <dewayneallen76@gmail.com>
2025-06-17 13:45:51 -0500

    Day 5: for loops with range() function, guass challenge

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,10 @@
+for number in range(1, 10):
+    print(number)
+
+# Gauss challenge
+# work out the total of the numbers between 1 and 100
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
