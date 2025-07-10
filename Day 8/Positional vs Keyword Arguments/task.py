cbf7ac841bfad25df6bd56f49b2bd57941e0dd3f
Dewayne Allen <dewayneallen76@gmail.com>
2025-07-10 16:02:38 -0400

    Day 8: Functions with more than 1 input, positional vs keyword argument

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -4,5 +4,15 @@ def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
-
 greet_with_name("Jack Bauer")
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"How is it in {location}?")
+
+greet_with("Dewayne", "Riceville")
+
+# Keyword arguments
+greet_with(name="Kristy", location="Cleveland")
+
