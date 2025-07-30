59f60fab59a974eb4378cecee2f4a7774266bac9
Dewayne Allen <dewayneallen76@gmail.com>
2025-07-30 16:23:06 -0400

    Day 10: Docstrings. Using docstrings to define a function. Adding a docstring and hovering over the self-created function to see the definition of what the function does.

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """Takes a first and last name and format it to return the title case
+    version of the name"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
@@ -6,7 +8,10 @@ def format_name(f_name, l_name):
 
 formatted_name = format_name("AnGeLa", "YU")
 
-length = len(formatted_name)
+
+def multiply(num_1, num_2):
+    """Multiplies num_1 and num_2"""
+    return num_1 * num_2
 
 
-
+print(multiply(3, 5))
