201850c173ba5de885a86662e159dc73942f0185
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-29 16:56:10 -0400

    Day 12: Scope, block scope

---
+++ Day 12/Block Scope/task.py
@@ -0,0 +1,14 @@
+# There is no Block Scope in Python!
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+
+def create_enemy():
+    new_enemy = ""
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
+
+# This will result in a NameError because it is out of the scope of the function
+# print(new_enemy)
