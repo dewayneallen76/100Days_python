40d1a406af18f3c251ed8dd74c00aa1742723ad2
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-29 17:13:51 -0400

    Day 12: Scope, global constants.

---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,15 @@
+# Global Constants
+
+# You can define global constants in your code file for easy access. Their job is
+# meant to be "set and forget" so you can use their values but never need to modify
+# them.
+
+# Global constants are normally declared in ALL_CAPS with a underscore in between.
+
+PI = 3.14159
+GOOGLE_URL = "https://www.google.com"
+
+def my_func():
+    print(GOOGLE_URL)
+
+my_func()
