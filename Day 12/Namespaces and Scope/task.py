857b9808ecd42a7d7fac4b7c4aab8d3a3b121a24
Dewayne Allen <dewayneallen76@gmail.com>
2025-09-29 16:39:09 -0400

    Day 12: Scope, local vs. global scope

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,20 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+
+# Global Scope
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength, player_health)
+
+drink_potion()
+print(player_health)
