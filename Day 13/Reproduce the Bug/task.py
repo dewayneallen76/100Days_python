2be2b9c7676aa911465b94479f6998dda795f866
Dewayne Allen <dewayneallen76@gmail.com>
2025-10-31 15:41:13 -0400

    Day 13: Debugging, reproduce the bug

--- Day 13/Reproduce the Bug/task.py
+++ Day 13/Reproduce the Bug/task.py
@@ -1,4 +1,7 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 6)
+dice_num = randint(0, 5)
 print(dice_images[dice_num])
+
+# Solution: randint was only looking between 1 and 6. The dice images are an array
+# it should start at 0 and end at 5.
